0bbc218d054a7dd212cb64188dd1719967398a40
Angel Aguirre <aaguirr7@ucsc.edu>
2025-09-04 18:43:42 -0700

    FINAL COMMIT

--- Assignment_1/README.md
+++ Assignment_1/README.md
@@ -6,7 +6,7 @@
 The program will ask you to enter the first number.
 After that the program will ask you to enter the second number.
 
-**Note:** both numbers must be integers. You will be asked to enter both numbers again if you enter another type.
+**Note:** both numbers must be integers. You will be asked to enter the value again if you enter something other than an integer
 
 Next the program will ask you to select an operation.
 You can select the following:
@@ -16,6 +16,7 @@ You can select the following:
 - Multiplication (Enter 3)
 - Division (Enter 4)
 
-**Note:** No other operations are supported. If you attempt to enter anything else the program will finish and it will ask if you want to do another calculation
+**Note:** No other operations are supported. If you attempt to enter anything else the program will tell that the operation is not supported and will ask you to select an operation.
+**Note:** You cannot divide by 0. If you try this the program will let you know and ask you to select an operation.
 
 Once operation is selected you will be able to see the result. The program will prompt you if you want to perform another calculation. If you type yes the program will start from the beginning. If you type no the program will say goodbye and terminate          
--- Assignment_1/code.py
+++ Assignment_1/code.py
@@ -5,43 +5,64 @@ print("Welcome to the Simple Calculator!")
 
 while(another):
 
- try:
-  num1 = int(input("Enter the first number: "))
-  num2 = int(input("Enter the second number: "))
- except ValueError:
-  print("This calculator only supports integers")
-  continue
- except:
-  print("Something went wrong")
+ while(True):
+  try:
+   num1 = int(input("Enter the first number: "))
+   break
+  except ValueError:
+   print("This calculator only supports integers")
+   continue
+  except:
+   print("Something went wrong")
+   
+ while(True):
+  try:
+   num2 = int(input("Enter the second number: "))
+   break
+  except ValueError:
+   print("This calculator only supports integers")
+   continue
+  except:
+   print("Something went wrong")
  
- print("Select an Operation:\n\
+
+ while(True):
+  print("Select an Operation:\n\
 1. Addition\n\
 2. Subtraction\n\
 3. Multiplication\n\
-4. Division")
+4. Division") 
  
- operation = int(input("Enter the operation number: "))
- 
- match operation:
-  case 1:
-   result = num1 + num2
-   print(f"{num1} + {num2} = {result}")
-  case 2:
-   result = num1 - num2
-   print(f"{num1} - {num2} = {result}")
-  case 3:
-   result = num1 * num2
-   print(f"{num1} * {num2} = {result}")
-  case 4:
-   try:
-    result = num1 / num2
-    print(f"{num1} / {num2} = {result}")
-   except ZeroDivisionError:
-    print("Cant divide by 0")
-   except:
-    print("Something went wrong")
-  case _:
-   print("That is not a supported operation")
+  try:
+   operation = int(input("Enter the operation number: "))
+  except ValueError:
+   print("Please Enter an Integer")
+   continue
+
+  match operation:
+   case 1:
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+    break
+   case 2:
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+    break
+   case 3:
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+    break
+   case 4:
+    try:
+     result = num1 / num2
+     print(f"{num1} / {num2} = {result}")
+     break
+    except ZeroDivisionError:
+     print("Cant divide by 0")
+    except:
+     print("Something went wrong")
+   case _:
+    print("That is not a supported operation")
 
  while(True):
 
